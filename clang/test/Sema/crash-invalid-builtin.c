b6c21d203bc6d6b93f1929cd52446b84ca5a8a81
mygitljf <2410316423@qq.com>
2026-06-24 01:30:56 +0000

    [clang] Avoid nan payload crash

--- clang/test/Sema/crash-invalid-builtin.c
+++ clang/test/Sema/crash-invalid-builtin.c
@@ -2,3 +2,5 @@
 // PR23086
 
 __builtin_isinf(...); // expected-error {{type specifier missing, defaults to 'int'}} expected-error {{ISO C requires a named parameter before '...'}} // expected-error {{cannot redeclare builtin function '__builtin_isinf'}} // expected-note {{'__builtin_isinf' is a builtin with type 'int ()'}}
+
+char nanf_wide = __builtin_nanf(L""); // expected-error {{incompatible pointer types passing 'int[1]' to parameter of type 'const char *'}} expected-error {{initializer element is not a compile-time constant}}
